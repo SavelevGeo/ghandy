eeebe047b68b4766920d67fa039f40db8988e3d8
Georgij Savelev <savelevgeo.post@gmail.com>
2022-06-09 13:52:35 +0000

    try set up crs for yandex. problems: yandex bad crs, osm not visiblible in layerswitch

--- ghandy-pack/main.js
+++ ghandy-pack/main.js
@@ -10,6 +10,7 @@ import { get as getProjection } from 'ol/proj';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
+import OSM from 'ol/source/OSM';
 
 //for layer switcher
 import 'ol/ol.css';
@@ -17,7 +18,11 @@ import 'ol-layerswitcher/dist/ol-layerswitcher.css';
 import LayerSwitcher from 'ol-layerswitcher';
 
 //custom projection difining, UNTM Zone 56N used for an example
-proj4.defs("EPSG:32656","+proj=utm +zone=56 +datum=WGS84 +units=m +no_defs");
+//map projection
+proj4.defs("EPSG:32656", "+proj=utm +zone=56 +datum=WGS84 +units=m +no_defs");
+//yandex sat projection
+proj4.defs("EPSG:3395",
+    "+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs");
 register(proj4);
 
 //map init
@@ -38,14 +43,18 @@ const map = new Map({
         new TileLayer({ //Yandex Maps
             source: new XYZ({
                 attributions:
-                    'Tiles © <a href="https://yandex.ru/legal/maps_termsofuse/>'
+                    'Tiles © <a href="https://yandex.ru/legal/maps_termsofuse/">'
                     + 'Yandex Satellite</a>',
                 url:
                     'https://core-sat.maps.yandex.net/tiles?l=sat&v=3.927.0' +
                     '&x={x}&y={y}&z={z}&scale=1',
+                projection: getProjection('EPSG:3395')
             }),
             title: 'Yandex Maps'
-        })
+        }),
+        new TileLayer({
+            source: new OSM(),
+    })
     ],
     view: new View({
         projection: getProjection('EPSG:32656'),
