a2aeed23c6a9ec6e6be67d4ab44d90149a30033c
Georgij Savelev <savelevgeo.post@gmail.com>
2022-06-09 09:00:21 +0000

    changing the projecting to UTM zone worked, to GK zone - not

--- ghandy-pack/main.js
+++ ghandy-pack/main.js
@@ -2,16 +2,23 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import proj4 from 'proj4';
+import {register} from 'ol/proj/proj4';
+import {get as getProjection} from 'ol/proj';
+
+proj4.defs("EPSG:32656","+proj=utm +zone=56 +datum=WGS84 +units=m +no_defs");
+register(proj4);
 
 const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
+    target: 'map',
+    layers: [
+        new TileLayer({
+            source: new OSM()
+        })
+    ],
+    view: new View({
+        projection: getProjection('EPSG:32656'),
+        center: [483312.3,7024735.4],
+        zoom: 10
   })
 });
