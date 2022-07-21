e650b31e802f6aa6c344f93ece1f80a83d1bedd2
Georgij Savelev <savelevgeo.post@gmail.com>
2022-07-21 08:14:54 +0300

    vite build inserted into cordova

--- cordova-ghandy/ghandy/www/js/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
